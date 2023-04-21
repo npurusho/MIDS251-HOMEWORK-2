65f1ee262770283fc6d79ab59e165e5ee9cc786f
Naveen Purushotham <pnaveennischal@gmail.com>
2023-04-21 07:58:39 -0700

    Add files via upload

---
+++ capture/cam.py
@@ -0,0 +1,32 @@
+# cam.py
+# this is from https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+# the index depends on your camera setup and which one is your USB camera.
+# you may need to change to 1 or 2 depending on your machine.
+cap = cv2.VideoCapture(0) # with macOS and an iphone, this might be your iphone camera
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+       cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+       roi_gray = gray[y:y+h, x:x+w]
+       roi_color = frame[y:y+h, x:x+w]
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(30) & 0xFF == ord('q'):
+        break
+
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
